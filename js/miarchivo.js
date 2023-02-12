0481bc7e0b3d0da5c63add7de72f6f827cfb5de5
Micaela Frylingsztein <micafry@gmail.com>
2023-02-12 18:19:58 -0300

    do while agregado

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -3,31 +3,34 @@ const euro = 0.0049;
 const real = 0.027;
 
 
-let cifra = Number(prompt("Ingresar la cifra en pesos"))
-console.log("La cifra en pesos es " + cifra);
+let continuar;
 
-let moneda = prompt("Ingresar moneda a la cual desea convertir (dolar/real/euro)")
+do {
+    let cifra = Number(prompt("Ingresar la cifra en pesos"))
+    console.log("La cifra en pesos es " + cifra);
+
+    let moneda = prompt("Ingresar moneda a la cual desea convertir (dolar/real/euro)")
+
 
     if (moneda == "dolar") {
-    resultado = cifra * dolar
-    document.write(cifra + " ARS = " + resultado + " USD");
-}
-else if (moneda == "euro") {
-    resultado = cifra * euro
-    document.write(cifra + " ARS = " + resultado + " EUR");
-}
-else if (moneda == "real") {
-    resultado = cifra * real
-    document.write(cifra + " ARS = " + resultado + " BRL");
-}
-else { document.write("No contamos con esa moneda en el conversor. Por favor ingrese una de las siguientes opciones: dolar, euro o real.") }
-console.log("La moneda elegida es " + moneda)
+        resultado = cifra * dolar
+        document.write(cifra + " ARS = " + resultado + " USD <br>");
+    }
+    else if (moneda == "euro") {
+        resultado = cifra * euro
+        document.write(cifra + " ARS = " + resultado + " EUR <br>");
+    }
+    else if (moneda == "real") {
+        resultado = cifra * real
+        document.write(cifra + " ARS = " + resultado + " BRL <br>");
+    }
+    else { document.write("No contamos con esa moneda en el conversor. Por favor ingrese una de las siguientes opciones: dolar, euro o real.") }
 
-/* while (cifra != "ESC"){
-    cifra= prompt ("Ingresar otra cifra o escribir ESC para salir")
-}
- no sé cómo hacer acá porque no me va sumando los datos uno abajo del otro onda queda solo una conversión :((( q hagoooo */
+    console.log("La moneda elegida es " + moneda)
 
+    continuar = prompt("¿Desea continuar? SI/NO").toUpperCase()
+
+} while (continuar != "NO")
 
 
 
